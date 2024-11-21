5ad1a8ab4c520f34b7ecb84ad11d9700fee99f50
mohamed-moslemani <moslemanomohamed@gmail.com>
2024-11-21 23:53:45 +0200

    updating main app look

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Signup from "./pages/signup";
-import Login from "./pages/login";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import HomePage from "./pages/home"; // Example page
+import LoginPage from "./pages/login"; // Example page
+import SignupPage from "./pages/signup"; // Example page
+import AboutPage from "./pages/about"; // Example page
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Manage login state
+
+    return (
+        <Router>
+            <div className="app-container">
+                <Navbar isLoggedIn={isLoggedIn} />
+                <Routes>
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    {/* Add more routes here */}
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
 
 export default App;
