e17b4a8b51101951f0bafce0d1fac2cfcc7ebe30
mohamed-moslemani <moslemanomohamed@gmail.com>
2024-11-21 21:35:45 +0200

    pages for frontend setup

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./login.css"
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    alert(data.message || data.error);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default Login;
