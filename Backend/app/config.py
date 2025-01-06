799a15a964d1f254bc018dff5b51764ab02a2f2e
mohamed-moslemani <moslemanomohamed@gmail.com>
2025-01-06 23:52:36 +0200

    updating database route

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -4,8 +4,8 @@ import os
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/myapp")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-secret-key")
-    ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY", "your-encryption-key")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/brainstormer")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
     MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
     MISTRAL_AGENT_ID = os.getenv("MISTRAL_AGENT_ID")
